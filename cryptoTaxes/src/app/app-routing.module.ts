54c634ba0cfd1cdadf39a643c79624768252fa1b
JoshSevy <joshuasevy@outlook.com>
2022-02-19 11:52:10 -0700

    Add nav container stying

--- cryptoTaxes/src/app/app-routing.module.ts
+++ cryptoTaxes/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderNavComponent } from './header-nav/header-nav.component';
+import { HomepageContainerComponent } from './homepage-container/homepage-container.component';
+import { TipsComponent } from './shared/tips/tips.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '', component: HeaderNavComponent },
+  { path: '', component: HomepageContainerComponent },
+  { path: 'tips', component: TipsComponent },
+  { path: '**', redirectTo: '' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- cryptoTaxes/src/app/app.module.ts
+++ cryptoTaxes/src/app/app.module.ts
@@ -2,19 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HeaderNavModule } from './header-nav/header-nav.module';
 import { AppComponent } from './app.component';
-import { HeaderNavComponent } from './header-nav/header-nav.component';
+import { HomepageModule } from './homepage-container/homepage.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderNavComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, HeaderNavModule, HomepageModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
