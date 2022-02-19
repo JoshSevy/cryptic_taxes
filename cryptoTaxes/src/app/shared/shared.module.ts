559251531263099d3fae971b995a8170f3e71055
JoshSevy <joshuasevy@outlook.com>
2022-02-19 11:41:38 -0700

    Add tips component

--- cryptoTaxes/src/app/shared/shared.module.ts
+++ cryptoTaxes/src/app/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoadingSpinnerComponent } from './loading-spinner/loading-spinner.component';
+import { TipsComponent } from './tips/tips.component';
 
 
 
 @NgModule({
   declarations: [
-    LoadingSpinnerComponent
+    LoadingSpinnerComponent,
+    TipsComponent
   ],
   imports: [
     CommonModule
